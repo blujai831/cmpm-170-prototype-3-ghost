af45124d33c7cb93f51974f4ce27ef214ad4830f
blujai831 <webmaster@blujai831.dev>
2024-11-11 13:24:04 -0800

    Replace placeholder triangle with player character prefab. Delete placeholder circle triangle in preparation to replace it with ghost prefab.

---
+++ Assets/Scripts/PlayerCharacterController.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerCharacterController : MonoBehaviour
+{
+    [SerializeField] private GameObject _flashlight;
+    [SerializeField] private float _flashlightTurnSpeed;
+
+    private GridConstrainedMotion2D _mover;
+    private FramewiseAnimator _animator;
+    private InputAction _moveAction;
+    private int _powerupTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _mover = GetComponent<GridConstrainedMotion2D>();
+        _animator = GetComponent<FramewiseAnimator>();
+        _moveAction = InputSystem.actions.FindAction("move");
+        _powerupTimer = -1;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        ProcessMoveInput();
+        UpdateAnimation();
+        UpdateFlashlightAngle();
+    }
+
+    private void ProcessMoveInput() {
+        var moveValue = _moveAction.ReadValue<Vector2>();
+        if (moveValue.magnitude > 0.0f) {
+            Direction2D direction;
+            if (Math.Abs(moveValue.x) < Math.Abs(moveValue.y)) {
+                if (moveValue.y > 0) {
+                    direction = Direction2D.Up;
+                } else {
+                    direction = Direction2D.Down;
+                }
+            } else {
+                if (moveValue.x < 0) {
+                    direction = Direction2D.Left;
+                } else {
+                    direction = Direction2D.Right;
+                }
+            }
+            _mover.TryMove(direction);
+        }
+    }
+
+    private string GetCorrectAnimation() {
+        bool poweredUp = _powerupTimer > 0;
+        bool moving = _mover.Moving();
+        if (!poweredUp && !moving) return "Stand";
+        if (!poweredUp && moving) return "Walk";
+        if (poweredUp && !moving) return "Stand Powerup";
+        if (poweredUp && moving) return "Walk Powerup";
+        Contract.Assert(false); return "";
+    }
+
+    private void UpdateAnimation() {
+        _animator.CurrentAnimation = GetCorrectAnimation();
+        _animator.FacingDirection = _mover.FacingDirection;
+    }
+
+    private void UpdateFlashlightAngle() {
+        var targetQ = Quaternion.identity;
+        switch (_animator.FacingDirection) {
+            case Direction2D.Up:
+                targetQ = Quaternion.Euler(0.0f, 0.0f, 180.0f);
+                break;
+            case Direction2D.Left:
+                targetQ = Quaternion.Euler(0.0f, 0.0f, -90.0f);
+                break;
+            case Direction2D.Right:
+                targetQ = Quaternion.Euler(0.0f, 0.0f, 90.0f);
+                break;
+        }
+        _flashlight.transform.rotation = Quaternion.Slerp(
+            _flashlight.transform.rotation,
+            targetQ,
+            Time.deltaTime*_flashlightTurnSpeed
+        );
+    }
+}

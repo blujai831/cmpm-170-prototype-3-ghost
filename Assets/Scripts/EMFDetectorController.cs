d9d697f96276ac80516a211e3fd90764c257634a
blujai831 <webmaster@blujai831.dev>
2024-11-11 17:59:46 -0800

    Add EMF detector object and test UI for it.

---
+++ Assets/Scripts/EMFDetectorController.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EMFDetectorController : MonoBehaviour
+{
+    private List<GameObject> _enemiesInRange;
+    private float _shortestDistance;
+
+    void Start() {
+        _enemiesInRange = new List<GameObject>();
+        _shortestDistance = Single.PositiveInfinity;
+    }
+
+    void FixedUpdate() {
+        RecalcShortestDistance();
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        var gobj = other.gameObject;
+        if (
+            !_enemiesInRange.Contains(gobj) &&
+            gobj.GetComponent<EnemyController>() != null
+        ) {
+            _enemiesInRange.Add(gobj);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other) {
+        _enemiesInRange.Remove(other.gameObject);
+    }
+
+    private void RecalcShortestDistance() {
+        _shortestDistance = Single.PositiveInfinity;
+        foreach (var enemy in _enemiesInRange) {
+            float candidate =
+                Vector3.Distance(transform.position, enemy.transform.position);
+            if (candidate < _shortestDistance) {
+                _shortestDistance = candidate;
+            }
+        }
+    }
+
+    public int Count {get => _enemiesInRange.Count;}
+    public float ShortestDistance {get => _shortestDistance;}
+}
